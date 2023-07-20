bd5d6bd005d38d930c7d3816a5a84a0ac02fc3c9
KeisukeHara <139297343+KeisukeHara@users.noreply.github.com>
2023-07-20 11:11:02 +0900

    Add files via upload

--- input_app.py
+++ input_app.py
@@ -11,19 +11,19 @@ st.title("金利先物オプション時価計算画面")
 
 # 取引データの入力フォーム
 st.header("取引データ入力")
-strike_price = st.number_input("ストライク価格", value=100.0, step=0.1)
+strike_price = st.number_input("ストライク価格", value=100.0, step=0.1, format="%f")
 expiration_date = st.text_input("オプションの満期日", value="20230914")
-amount = st.number_input("額面", value=1000000)
+amount = st.number_input("額面", value=1000000.0, format="%f")
 call_put = st.selectbox("Call/Put", ["C", "P"])
 buy_sell = st.selectbox("Buy/Sell", ["B", "S"])
 
 # マーケットデータの入力フォーム
-st.header("マーケットデータ入力")
-evaluation_date = st.text_input("評価日", value="20230712")
-spot_date = st.text_input("スポット日", value="20230714")
-interest_rate = st.number_input("金利", value=0.01, step=0.001)
-volatility = st.number_input("ボラティリティ", value=0.2, step=0.001)
-underlying_price = st.number_input("原資産価格", value=105.0, step=0.001)
+st.sidebar.header("マーケットデータ入力")
+evaluation_date = st.sidebar.text_input("評価日", value="20230712")
+spot_date = st.sidebar.text_input("スポット日", value="20230714")
+interest_rate = st.sidebar.number_input("金利", value=0.01, step=0.001, format="%f")
+volatility = st.sidebar.number_input("ボラティリティ", value=0.2, step=0.001, format="%f")
+underlying_price = st.sidebar.number_input("原資産価格", value=105.0, step=0.001, format="%f")
 
 # 計算リクエスト
 body = {
@@ -45,9 +45,9 @@ body = {
   }
 }
 
-res = requests.post(URL, json.dumps(body))
-
 # 計算結果の表示
-st.header("計算結果")
-st.write("premium:", res.json()["premium"])
-st.write("pv:", res.json()["pv"])
+if st.button("計算実行"):
+  res = requests.post(URL, json.dumps(body))
+  st.header("計算結果")
+  st.write("premium:", res.json()["premium"])
+  st.write("pv:", res.json()["pv"])
